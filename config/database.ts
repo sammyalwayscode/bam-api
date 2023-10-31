9e53e1a15b4a1b4c4fe9217f1485d349a22633e7
sammyalwayscode <olorundasamuel2@gmail.com>
2023-10-31 16:11:28 +0100

    rounding up

--- config/database.ts
+++ config/database.ts
@@ -1,10 +1,12 @@
 import mongoose from "mongoose";
 
 const DB_URL = "mongodb://0.0.0.0:27017/newsFeedDatabase";
+const atlasURI =
+  "mongodb+srv://Sammy:Sammy@cluster0.aqlrqqw.mongodb.net/allStar?retryWrites=true&w=majority";
 
 export const dbConfig = async () => {
   try {
-    const dbConnect = await mongoose.connect(DB_URL);
+    const dbConnect = await mongoose.connect(atlasURI);
     console.log(`connected to ${dbConnect.connection.host}`);
   } catch (error) {
     console.log(`failed to connect to database`, error);
